d510385161024eae342733d638726bd580fda657
itsanat0m <mikkelsen.c.adam@gmail.com>
2026-02-01 14:20:00 -0500

    worked on front page direction with designer consultation

--- MikkelSite/src/mainPage/MainPage.tsx
+++ MikkelSite/src/mainPage/MainPage.tsx
@@ -9,9 +9,27 @@ function MainPage() {
         <img src="idk" />
       <Parallax translateY={['100%', '-100%']}>
       <div className="mainBlurb">
-        <div style={{minHeight: 800}}>
+        <div className="aboutMe">
+          <img src="idk" />
+          <div>
+            <h1>About Me</h1>
+            <p>I am a student at UNC Chapel Hill studying comuter science.</p>
+          </div>
         </div>
-
+        <div className="workExp">
+          <img src="idk" />
+          <div>
+            <h1>Work Experience</h1>
+            <p>Dude I lowkey got like no real CS experience im so cooked.</p>
+          </div>
+        </div>
+        <div className="school">
+          <img src="idk" />
+          <div>
+            <h1>Schooling</h1>
+            <p>Im so done with school oh my god get me the fuck OUTTA HERE</p>
+          </div>
+        </div>
         <NavBar />
       </div>
       </Parallax>
